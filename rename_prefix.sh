908e41acaab65b3c19b760dfb93114487b62427b
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2022-03-15 12:24:20 +0000

    Update rename_prefix.sh

--- rename_prefix.sh
+++ rename_prefix.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
-# Usage: addprefix <prefix> <files>
+# To rename file names by adding a prefix, can be used in a for loop
+usage () {
+  echo ""
+  echo "This bash script renames files by adding a prefix"
+  echo ""
+  echo "Usage: $0 [options] NEWPREFIX filenames"
+  echo ""
+  echo "e.g. $0 ABC bin.*.fasta"
+  echo ""
+  echo "Options:"
+  echo " -h print usage and exit"
+  echo " -a print author and exit"
+  echo " -v print version and exit"
+  echo ""
+}
+version () { echo "version 1.0";}
+author () { echo "Author: Raymond Kiu Raymond.Kiu@quadram.ac.uk";}
 
+while getopts ':o:hav' opt;
+do
+  case $opt in
+    h) usage; exit;;
+    a) author; exit;;
+    v) version; exit;;
+    \?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
+    :) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+    *) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+
+esac
+done
 prefix=$1
 shift
 for f in "$@"

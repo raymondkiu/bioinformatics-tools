819e197a8af0f691ca023966aa6e9d635942f49f
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2023-03-27 12:36:30 +0100

    Create basename-one-liner.sh

---
+++ basename-one-liner.sh
@@ -0,0 +1,5 @@
+# e.g. BA1_R1.fastq BA2_R1.fastq BA3_R1.fastq
+# Using this one-liner command will generate:
+# $ basename-one-liner.sh _R1.fastq
+# BA1 BA2 BA3
+for j in *$1 ; do basename $j $1 ;done |sed ':a;N;$!ba;s/\n/ /g'

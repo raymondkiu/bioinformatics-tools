4147ce1fc75a14f36b3b54a872c1f9abef5cf952
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2023-03-27 12:37:23 +0100

    Delete basename_dir.sh

--- basename_dir.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Raymond Kiu Raymond.Kiu@quadram.ac.uk
-# a one-line script to print basename of files with same suffixes for various purposes.
-for j in *$1 ; do basename $j $1 ;done |sed ':a;N;$!ba;s/\n/ /g'
